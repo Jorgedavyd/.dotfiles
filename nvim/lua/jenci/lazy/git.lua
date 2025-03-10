6f63c609ff14f61aff8bb9223ad65e9903278f6b
Jorgedavyd <jorged.encyso@gmail.com>
2025-03-10 08:28:23 -0300

    default

--- nvim/lua/jenci/lazy/git.lua
+++ nvim/lua/jenci/lazy/git.lua
@@ -9,32 +9,5 @@ return {
         vim.keymap.set("n", "<leader>p", function()
             vim.cmd.Git('push')
         end)
-        --- Setting up diffview
-        vim.keymap.set("n", "<leader>gd", function()
-            local lib = require('diffview.lib')
-            local view = lib.get_current_view()
-            if view then
-                vim.cmd("DiffviewClose")
-            else
-                vim.cmd("DiffviewOpen")
-            end
-        end)
-        vim.keymap.set("n", "]g", "<cmd>silent! /^<<<<<<< \\|^=======\\|^>>>>>>><CR>", { silent = true })
-        vim.keymap.set("n", "[g", "<cmd>silent! ?^<<<<<<< \\|^=======\\|^>>>>>>><CR>", { silent = true })
-        vim.keymap.set("n", "<leader>gb", ":Git blame<CR>")
-        vim.keymap.set("n", "<leader>gl", ":0Gclog<CR>")
-
-        vim.keymap.set("n", "<leader>gn", function()
-            local found = vim.fn.search('^<<<<<<< \\|^=======\\|^>>>>>>>', 'W')
-            if found > 0 then
-                local line = vim.fn.getline('.')
-                if string.match(line, '^<<<<<<< ') then
-                    vim.cmd('normal! jdd/^=======\\|^>>>>>>>\nd/^>>>>>>>\nd')
-                elseif string.match(line, '^=======') then
-                    vim.cmd('normal! dd/^>>>>>>>\nd')
-                end
-                return
-            end
-        end)
     end,
 }
