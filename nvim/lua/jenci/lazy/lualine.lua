356adc81581360ad4cac071f78b1ec01865364b5
Jorgedavyd <jorged.encyso@gmail.com>
2025-03-10 16:01:06 -0300

    setting up the styles

--- nvim/lua/jenci/lazy/lualine.lua
+++ nvim/lua/jenci/lazy/lualine.lua
@@ -2,9 +2,12 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function ()
+        local custom = require("lualine.themes.auto")
+        custom.normal.c.bg = "#0d1011"
+        custom.normal.b.bg = "#1B2023"
         require('lualine').setup({
             options = {
-                theme = 'auto',
+                theme = custom,
                 component_separators = { left = '|', right = '|' },
                 section_separators = { left = '', right = '' },
                 disabled_filetypes = { 'packer', 'NVimTree' }
