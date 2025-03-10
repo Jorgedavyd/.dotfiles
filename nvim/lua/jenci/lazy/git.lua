91649fe683b37688fd58de468070043778781041
Jorgedavyd <jorged.encyso@gmail.com>
2025-03-10 08:29:49 -0300

    setting up basic vim fugitive for targeted managament

--- nvim/lua/jenci/lazy/git.lua
+++ nvim/lua/jenci/lazy/git.lua
@@ -1,13 +1,6 @@
 return {
     "tpope/vim-fugitive",
-    dependencies = {
-        "sindrets/diffview.nvim",
-    },
     config = function()
-        --- Setting up vim-fugitive
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        vim.keymap.set("n", "<leader>p", function()
-            vim.cmd.Git('push')
-        end)
     end,
 }
